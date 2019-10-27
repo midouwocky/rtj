0d859fea5064f3b29c43bdd743d27f655f74319f
ahmed <ahmdaou2808@gmail.com>
2019-10-27 23:01:30 +0000

    fix https problem

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-const serverUrl = 'http://young-scrubland-57448.herokuapp.com';
+const serverUrl = 'https://young-scrubland-57448.herokuapp.com';
 
 export const environment = {
   production: true,
